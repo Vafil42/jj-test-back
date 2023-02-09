4c256f84749ca9dd9bf8f869c953562c03c24980
Vafil42 <vafil6241@gmail.com>
2023-02-09 12:18:03 +0300

    database edit

--- dist/constants/index.d.ts
+++ dist/constants/index.d.ts
@@ -1,4 +1,4 @@
 export declare const SEQUELIZE = "SEQUELIZE";
-export declare const DEVELOPMENT = "postgres";
+export declare const DEVELOPMENT = "development";
 export declare const TEST = "test";
 export declare const PRODUCTION = "production";
