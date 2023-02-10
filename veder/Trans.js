6d55288f3ced2537f10a2ce0d0767a457ff8a57e
patrik200 <dalbab@inbox.ru>
2023-02-10 11:16:57 +0300

    vendor func

---
+++ veder/Trans.js
@@ -0,0 +1,15 @@
+
+    
+    
+    function trans(str) {
+    abc_lover = ["а-ac", "б-b", "в-v", "г-g", "д-d", "е-e", "ё-yo", "ж-zh", "з-z", "и-i", "й-yi", "к-k", "л-l", "м-m", "н-n", "о-o", "п-p", "р-r", "с-s", "т-t", "у-u", "ф-f", "х-kh", "ц-tc", "ч-ch", "ш-sh", "щ-shch", "ъ-\'", "ы-y", "ь\"", "э-e", "ю-yu", "я-ya"];
+    str1 = "";
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < abc_lover.length; j++) {
+            if (str[i] == abc_lover[j].substring(0, 1)) {
+                str1 = str1 + abc_lover[j].substring(2);
+            }
+        }
+    }
+    return str1;
+}
