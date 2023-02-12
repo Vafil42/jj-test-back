087e2f719f11cf78453a3fc5f20652fdfebca74a
patrik200 <dalbab@inbox.ru>
2023-02-12 21:24:02 +0300

    new vedor

--- veder/Trans.js
+++ veder/Trans.js
@@ -1,7 +1,4 @@
-
-    
-    
-    function trans(str) {
+export default function trans(str) {
     abc_lover = ["а-ac", "б-b", "в-v", "г-g", "д-d", "е-e", "ё-yo", "ж-zh", "з-z", "и-i", "й-yi", "к-k", "л-l", "м-m", "н-n", "о-o", "п-p", "р-r", "с-s", "т-t", "у-u", "ф-f", "х-kh", "ц-tc", "ч-ch", "ш-sh", "щ-shch", "ъ-\'", "ы-y", "ь\"", "э-e", "ю-yu", "я-ya"];
     str1 = "";
     for (let i = 0; i < str.length; i++) {
