ec3ee8da8a3bc142d606b91f891c7e3a3956814a
Vafil42 <vafil6241@gmail.com>
2023-02-14 12:28:02 +0300

    nothing

--- src/worker/worker.entity.ts
+++ src/worker/worker.entity.ts
@@ -1,4 +1,4 @@
-import { Table, Column, PrimaryKey, Model } from 'sequelize-typescript';
+import { Table, Column, Model } from 'sequelize-typescript';
 
 @Table
 export class WorkerEntity extends Model<WorkerEntity> {
