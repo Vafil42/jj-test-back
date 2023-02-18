538386b79a5ca77f40d1ea22f0bfcf55e8dbf62d
patrik200 <dalbab@inbox.ru>
2023-02-18 11:13:37 +0300

    new vendor

--- src/vendor/random-str.js
+++ src/vendor/random-str.js
@@ -1,6 +1,7 @@
-str_lover_engl_and_num = ["a", "b", "c", "d", 
-"e", "f", "g", "h", "i", "j", "k" ,"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v" ,"w", "x" ,"y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 function randomStructuredClone(count){
+    str_lover_engl_and_num = ["a", "b", "c", "d", 
+        "e", "f", "g", "h", "i", "j", "k" ,"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v" ,"w", "x" ,"y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
     str = '';
     if (count == null){
         count = 5;
