28ffeb8cf39b68c6fc754eda4e9ebe61eaa77318
patrik200 <dalbab@inbox.ru>
2023-02-18 13:24:40 +0300

    update vendor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
-import { UserModule } from './user/user.module'
+import { UserModule } from './user/user.module';
 
 @Module({
-    imports: [ConfigModule.forRoot({isGlobal: true}), DatabaseModule, UserModule],
-    controllers: [],
-    providers: [],
-    exports: []
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
+  exports: [],
 })
 export class AppModule {}
