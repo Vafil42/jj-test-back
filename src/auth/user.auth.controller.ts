6cd346c272a01372af267929a344e27c998f13e8
a-aniskin <a.aniksin2006@gmail.com>
2023-02-25 17:28:21 +0300

    Swagger updated

--- src/auth/user.auth.controller.ts
+++ src/auth/user.auth.controller.ts
@@ -4,23 +4,29 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { JwtAuthGuard } from './guard/jwt-auth.guard';
 import { LocalAuthGuard } from './guard/local-auth.guard';
 import { UserService } from '../user/user.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { LoginUserDto } from './dto/login-user.dto';
 
+@ApiTags('Пользователи')
 @Controller('auth')
 export class UserAuthController {
   constructor(private userService: UserService) {}
 
+  @ApiOperation({ summary: 'Аутентификация пользователя' })
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return this.userService.login(req.user);
   }
 
+  @ApiOperation({ summary: 'Аторизация пользователя' })
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return this.userService.login(req.user);
   }
 
+  @ApiOperation({ summary: 'Регистрация пользователя' })
   @Post('create')
   async create(@Body() dto: CreateUserDto) {
     return this.userService.create(dto);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,21 +3,26 @@ import { Get, Post, Delete, Put } from '@nestjs/common';
 import { CreateUserDto } from '../auth/dto/create-user.dto';
 import { UpdateUserDto } from '../auth/dto/update-user.dto';
 import { UserService } from './user.service';
+import { ApiTags, ApiProperty, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('Какие-то еще пользователи')
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @ApiOperation({ summary: 'Получение списка пользователей' })
   @Get()
   async findAll() {
     return await this.userService.findAll();
   }
 
+  @ApiOperation({ summary: 'Получение данных об указанном пользователе' })
   @Get(':id')
   async findById(@Param() param) {
     return await this.userService.findById(param.id);
   }
 
+  @ApiOperation({ summary: '' })
   @Post()
   async create(@Body() dto: CreateUserDto) {
     this.userService.create(dto);
