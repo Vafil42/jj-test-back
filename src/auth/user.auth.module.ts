5d9be41f6504faad84e5d4766769cba02996dc72
Vafil42 <123875901+Vafil42@users.noreply.github.com>
2023-05-03 11:15:24 +0000

    delete dist

--- src/auth/user.auth.module.ts
+++ src/auth/user.auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@Nestjs/common';
+import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
@@ -17,7 +17,9 @@ dotenv.config();
         PassportModule,
         JwtModule.register({ secret: process.env.JWT_SECRET }),
     ],
+
     controllers: [UserAuthController],
+    
     providers: [
         LocalStrategy,
         JwtUserStrategy,
